9b9b7b4cb647acbf6d1056148884043a33d1b8f3
Andrey1914 <anlikajw@gmail.com>
2023-04-02 22:01:14 +0200

    created reviews

---
+++ src/components/reviews/index.vue
@@ -0,0 +1,69 @@
+<template>
+    <section class="reviews">
+        <div class="reviews__heading">
+            <h2 class="reviews__title">Overall rating</h2>
+            <div class="reviews__rating">
+                <span>{{ amountOfReviews }} Feedback(s)</span>
+                <Rating :rating="rating" />
+            </div>
+        </div>
+        <ReviewsItem v-for="review in reviews" :key="review.author" :review="review" />
+
+    </section>
+</template>
+
+<script>
+import ReviewsItem from './reviews-item/index.vue';
+import Rating from '../StarRating.vue';
+
+export default {
+    name: 'ReviewsItem',
+    components: {
+        ReviewsItem,
+        Rating
+    },
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables.scss";
+
+.reviews {
+    margin-top: 20px;
+    background: $card-bg;
+    padding: 0 2px;
+
+    &__heading {
+        padding: 20px 20px 10px;
+    }
+
+    &__title {
+        text-transform: uppercase;
+        font-size: 20px;
+        font-weight: 700;
+        margin-bottom: 10px;
+    }
+
+    &__rating {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    &__show-more {
+        background: none;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        padding: 12px;
+        font-family: inherit;
+        font-size: 16px;
+    }
+}
+</style>
--- src/pages/Apartment.vue
+++ src/pages/Apartment.vue
@@ -3,7 +3,10 @@
         <Container>
             <div class="apartment-page__content">
                 <ApartmentsMainInfo :apartment="apartment" />
-                <ApartmentsOwner class="apartment-page__owner" :owner="apartment.owner" />
+                <div class="apartment-page__additional-info">
+                    <ApartmentsOwner class="apartment-page__owner" :owner="apartment.owner" />
+                    <Reviews :reviews="reviewsList" />
+                </div>
             </div>
         </Container>
     </main>
@@ -14,14 +17,20 @@ import Container from '@/components/Shared/Container.vue';
 import apartments from '@/components/Apartment/apartments';
 import ApartmentsMainInfo from '@/components/Apartment/ApartmentsMainInfo.vue';
 import ApartmentsOwner from '@/components/Apartment/ApartmentsOwner.vue';
+import Reviews from '../components/reviews';
+import reviewsList from '../components/reviews/reviews';
 export default {
     name: 'ApartmentPage',
     components: {
         Container,
         ApartmentsMainInfo,
-        ApartmentsOwner
+        ApartmentsOwner,
+        Reviews,
     },
     computed: {
+        reviews() {
+            return reviewsList;
+        },
         apartment() {
             return apartments.find(apartment => apartment.id === this.$route.params.id)
         }
